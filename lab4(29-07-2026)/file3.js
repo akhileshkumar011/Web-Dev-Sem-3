9309667829e488146d6a10df7d8f67ac7f82d4b4
akhileshkumar011 <akhileshkumar31806@gmail.com>
2026-07-29 12:15:02 +0530

    lab work

--- lab4(29-07-2026)/file3.js
+++ lab4(29-07-2026)/file3.js
@@ -1,22 +1,29 @@
-import { writeFile, readFile, appendFile } from "fs/promises";
+import { writeFile, readFile, appendFile, unlink } from "fs/promises";
 
-
-const writeData = async (fname , contents) => {
-    await writeFile(fname,contents);
-    console.log('File written');
-
+const writeData = async (fname, contents) => {
+    await writeFile(fname, contents);
+    console.log("File written");
 };
 
-const readData = async (fname)=>{
-    const data = await readFile(fname,'utf-8');
-    console.log('File contents');
+const readData = async (fname) => {
+    const data = await readFile(fname, "utf-8");
+    console.log("File contents:");
     console.log(data);
 };
-const appendData = async (fname ,contents) =>{
-    await appendFile(fname , "\n" + contents);
+
+const appendData = async (fname, contents) => {
+    await appendFile(fname, "\n" + contents);
+    console.log("Data appended");
 };
 
-await writeData("happy.txt"," I am very happy");
-await readData("happy.txt");
-await appendData("happy.txt" , "FSD is interesting");
-await readData("happy.txt");
+const deleteData = async (fname) => {
+    await unlink(fname);
+    console.log("File deleted");
+};
+
+// await writeData("happy.txt", "I am very happy");
+// await readData("happy.txt");
+// await appendData("happy.txt", "FSD is interesting");
+// await readData("happy.txt");
+
+await deleteData("happy.txt");
